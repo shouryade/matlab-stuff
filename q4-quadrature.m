7daabff548ab23e1e6992ff8ada33b87ec542bb3
shourya <67991164+shouryade@users.noreply.github.com>
2022-11-02 11:13:25 +0530

    added q4 soln of quadrature practical

---
+++ q4-quadrature.m
@@ -0,0 +1,21 @@
+clc
+clear all
+syms x
+g=@(x)((atan(1+(x^2))));
+f=inline(sqrt(1+(diff(g,x))^2));
+
+a=input("Enter value of a :");
+b=input("Enter value of b :");
+n=input("Enter number of subintervals :");
+h=(b-a)/n;
+sum=f(a)+f(b);
+for i=1:n-1
+    x=(a + (h*i));
+    if (rem(i,2)==0)
+        sum=sum+(2*f(x));
+    else
+        sum=sum+(4*f(x));    
+    end
+end
+c=sum*(h/3);
+disp(c)
